e3d955ec8dfa431d00c5ec3aefa28063b32209b3
Dmitry <pokemon2708@yandex.ru>
2024-12-13 23:02:36 +0300

    Create "Game" class

--- main.py
+++ main.py
@@ -5,40 +5,57 @@ SIZE = (1000, 650)
 CELL_SIZE = 50
 
 
-def main():
-    pygame.init()
-    pygame.display.set_caption('Castle')
-    screen = pygame.display.set_mode(SIZE)
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.running = True
+        self.fps = 100
+        self.clock = pygame.time.Clock()
+        self.create_screen()
+        self.create_world()
 
-    screen.fill(pygame.Color('black'))
-    pygame.display.flip()
+    def create_screen(self):
+        pygame.display.set_caption('Castle')
+        self.screen = pygame.display.set_mode(SIZE)
+        self.screen.fill(pygame.Color('black'))
 
-    world = pygame.sprite.Group()
-    tmx_map = load_pygame('world.tmx')
-    title_img = tmx_map.get_tile_image_by_gid
-    for layer in tmx_map.visible_layers:
-        if isinstance(layer, TiledTileLayer):
-            for x, y, serf in layer:
-                title = title_img(serf)
+    def create_world(self):
+        self.world = pygame.sprite.Group()
+        self.tmx_map = load_pygame('world.tmx')
+        title_img = self.tmx_map.get_tile_image_by_gid
+        for layer in self.tmx_map.visible_layers:
+            if isinstance(layer, TiledTileLayer):
+                for x, y, serf in layer:
+                    title = title_img(serf)
 
-                if title:
-                    title = pygame.transform.scale(title, (CELL_SIZE, CELL_SIZE))
-                    screen.blit(title, (x * CELL_SIZE, y * CELL_SIZE))
+                    if title:
+                        title = pygame.transform.scale(title, (CELL_SIZE, CELL_SIZE))
+                        self.screen.blit(title, (x * CELL_SIZE, y * CELL_SIZE))
 
-    running = True
-    fps = 100
-    clock = pygame.time.Clock()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    def set_darkness(self):
+        """Затемнение"""
+        darkness = pygame.Surface(SIZE)
+        darkness.fill(pygame.Color('black'))
+        darkness.set_alpha(100)
+        self.screen.blit(darkness, (0, 0))
 
-        world.draw(screen)
-        pygame.display.flip()
-        clock.tick(fps)
+    def run(self):
+        self.set_darkness()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-    pygame.quit()
+            self.world.draw(self.screen)
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+        pygame.quit()
+
+    def main(self):
+        self.run()
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.main()
